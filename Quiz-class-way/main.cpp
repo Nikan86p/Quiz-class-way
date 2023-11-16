a197d941c24f5a396278bdeb8d9cb54a3ad39d34
Nikan86p <nikan86p@gmail.com>
2023-11-16 15:23:57 +0330

    Initial Commit

---
+++ Quiz-class-way/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Quiz-class-way
+//
+//  Created by Nikan Pourreza on 11/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
