b5b3b5b5b16dcc3c1cedf405b77e88b287e0058a
Nikan86p <67387467+Nikan86p@users.noreply.github.com>
2023-11-24 13:18:34 +0330

    New Release: 1.1.1 :: (puting the code to order )

--- Quiz-class-way/main.cpp
+++ Quiz-class-way/main.cpp
@@ -22,10 +22,11 @@ class circle {
     
 private: double radius;
     
-public: double calculateArea_C(){
-   double C_area = radius*radius* M_PI;
-    return C_area;
-}
+public: 
+    double calculateArea_C(){
+        double C_area = radius*radius* M_PI;
+        return C_area;
+    }
     void setRadius (double r){
         radius = r;
     }
@@ -44,10 +45,11 @@ class rectangle {
 private: double Length , Width ;
     
     
-public: double calculateArea_R(){
-   double R_area = Length*Width;
-    return R_area;
-}
+public: 
+    double calculateArea_R(){
+        double R_area = Length*Width;
+        return R_area;
+    }
     void setL_W(double l , double w){
         Length = l ;
         Width = w ;
@@ -69,9 +71,10 @@ class shape : public circle , public rectangle {
 
 //private: double area;
   
-public: void Circle_Area(string subject){
-    cout << subject << calculateArea_C() << " cm2" ;
-}
+public: 
+    void Circle_Area(string subject){
+        cout << subject << calculateArea_C() << " cm2" ;
+    }
     void Rectangle_Area(string subject){
         cout << subject <<calculateArea_R() << " cm2\n" <<"***********\n";
     }
@@ -82,23 +85,30 @@ public: void Circle_Area(string subject){
 
 int main (){
     shape c = shape();
+    
     double m ;
     cout << "Enter the size of radius please : " ;
     cin >> m ;
+    
     c.setRadius(m);
     cout << "Circle radius: " << c.getRadius();
     cout << "\nProcessing....." ;
+    
     sleep(2);
     c.Circle_Area("\n\nCircle Area: ");
     cout << "\n***********";
+    
     double q , t ;
     cout << "\nEnter the size of length please : ";
     cin >> q ;
+    
     cout << "Enter the size of width please : ";
     cin >> t ;
+    
     c.setL_W(q, t);
     cout << "Rectangle: Lenght= " << c.getL() << "   Width=" << c.getW();
     cout << "\nProcessing....." ;
+    
     sleep(3);
     c.Rectangle_Area("\n\nRectangle Area: ");
     // return 0;
